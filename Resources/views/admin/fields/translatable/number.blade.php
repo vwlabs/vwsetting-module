a4da3eea917deebb54e128b6a52ff64a63966cab
KB More <kbmore231@gmail.com>
2020-07-30 12:14:18 +0530

    first commit

---
+++ Resources/views/admin/fields/translatable/number.blade.php
@@ -0,0 +1,10 @@
+<?php $defaultValue = isset($moduleInfo['default']) ? $moduleInfo['default']: ''; ?>
+<div class='form-group'>
+    {!! Form::label($settingName . "[$lang]", trans($moduleInfo['description'])) !!}
+    <?php if (isset($dbSettings[$settingName])): ?>
+        <?php $value = $dbSettings[$settingName]->hasTranslation($lang) ? $dbSettings[$settingName]->translate($lang)->value : $defaultValue; ?>
+        {!! Form::number($settingName . "[$lang]", old($settingName . "[$lang]", $value), ['class' => 'form-control', 'placeholder' => trans($moduleInfo['description'])]) !!}
+    <?php else: ?>
+        {!! Form::number($settingName . "[$lang]", old($settingName . "[$lang]", $defaultValue), ['class' => 'form-control', 'placeholder' => trans($moduleInfo['description'])]) !!}
+    <?php endif; ?>
+</div>
